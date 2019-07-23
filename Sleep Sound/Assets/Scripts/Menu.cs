5290ed10f748a47bbd4a6c958807937c4360d36c
microlinx2019 <hanyinhua2010@gmail.com>
2019-07-22 20:19:15 -0600

    Add unity files

---
+++ Sleep Sound/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private GameObject submenuParent;
+    public GameObject submenuPrefab;
+
+    public void Awake()
+    {
+        submenuParent = Instantiate(submenuPrefab);
+        submenuParent.transform.SetParent(transform,false);
+        submenuParent.SetActive(false);
+    }
+
+    public void ShowSubMenu()
+    {
+        submenuParent.SetActive(true);
+    }
+
+    public void HideSubMenu()
+    {
+        submenuParent.SetActive(false);
+    }
+}
---
+++ Sleep Sound/Assets/Scripts/SceneLoaderPre.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderPre : MonoBehaviour
+{
+    public void LoadPreviousScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+}
